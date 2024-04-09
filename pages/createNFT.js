55f99e4be71be6ae2e9bd9fac7ac7be21b368154
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-04-09 23:24:42 +0530

    Add form-data package to package.json and update PINATA_JWT in config.js

--- pages/createNFT.js
+++ pages/createNFT.js
@@ -1,126 +1,149 @@
-import {useState} from 'react'
-import {ethers} from 'ethers'
-import {create as ipfsHttpClient} from 'ipfs-http-client'
-import {useRouter} from 'next/router'
-import Web3Modal from'web3modal'
+import { useState } from "react";
+import { ethers } from "ethers";
+import { useRouter } from "next/router";
+import Web3Modal from "web3modal";
+import axios from "axios";
+// import fs from "fs";
+import FormData from "form-data";
 
-const {create} =require('ipfs-http-client')
-const projectId="" //<infura nft api>
-const projectSecret="" //<infura nft api>
+import { nftaddress, nftmarketaddress, PINATA_JWT } from "../config";
 
-const auth= "Basic " + Buffer.from(projectId + ":" +projectSecret).toString("base64");
-const client =create({
-  host: "ipfs.infura.io",
-  port:5001,
-  protocol:"https",
-  headers:{
-  authorization:auth
-  },
-});
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import NFTMarket from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
 
-import {
-  nftaddress,nftmarketaddress
-} from "../config"
+export default function CreateItem() {
+  const [fileUrl, setFileUrl] = useState(null);
+  const [formInput, updateFormInput] = useState({
+    price: "",
+    name: "",
+    description: "",
+  });
 
-import NFT from "../artifacts/contracts/NFT.sol/NFT.json"
-import NFTMarket from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json"
+  const router = useRouter();
 
-export default function CreateItem(){
-  const [fileUrl,setFileUrl] = useState(null)
-  const [formInput,updateFormInput]=useState({price:"",name:"",description:""})
-
-  const router=useRouter()
-  
-  async function onChange(e){
-    const file=e.target.files[0]
-    try{
-      const added=await client.add(
-        file,{
-          progress:(prog)=>console.log(`received:${prog}`)
+  async function onChange(e) {
+    const file = e.target.files[0];
+    try {
+      let data = new FormData();
+      data.append("file", file);
+      data.append("pinataOptions", '{"cidVersion":0}');
+      data.append("pinataMetadata", '{"name":"NFT"}');
+      const res = await axios.post(
+        "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${PINATA_JWT}`,
+          },
         }
-      )
-      const url=`https://ineuron-nft.infura-ipfs.io/ipfs/${added.path}`
-      setFileUrl(url)
-    }catch(error){
-      console.log("error uploading file, please try again:" ,error)
+      );
+      const url = `https://gateway.pinata.cloud/ipfs/${res.data.IpfsHash}`;
+      setFileUrl(url);
+    } catch (error) {
+      console.log("error uploading file, please try again:", error);
     }
   }
 
-  async function CreateMarket(){
-    const {name,description,price}=formInput
-    if(!name||!description||!price||!fileUrl) return
+  async function CreateMarket() {
+    const { name, description, price } = formInput;
+    if (!name || !description || !price || !fileUrl) return;
 
-    const data=JSON.stringify({
-      name,description,image:fileUrl
-    })
-    try{
-      const added=await client.add(data)
-      const url=`https://ineuron-nft.infura-ipfs.io/ipfs/${added.path}`
-      console.log(url)
-      createSale(url)
-    }catch(error){
-      console.log('error uploading file:',error)
+    const data = JSON.stringify({
+      pinataOptions: {
+        cidVersion: 0,
+      },
+      pinataMetadata: {
+        name: "testing",
+        keyvalues: {},
+      },
+      pinataContent: {
+        name,
+        description,
+        image: fileUrl,
+      },
+    });
+    try {
+      const res = await axios.post(
+        "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${PINATA_JWT}`,
+          },
+        }
+      );
+      const url = `https://gateway.pinata.cloud/ipfs/${res.data.IpfsHash}`;
+      console.log(url);
+      createSale(url);
+    } catch (error) {
+      console.log("error uploading file:", error);
     }
   }
 
-  async function createSale(url){
-    const web3Modal=new Web3Modal()
-    const connection=await web3Modal.connect()
-    const provider=new ethers.providers.Web3Provider(connection)
-    const signer=provider.getSigner()
+  async function createSale(url) {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
 
-    let contract=new ethers.Contract(nftaddress,NFT.abi,signer)
-    let transaction=await contract.createToken(url)
-    let tx=await transaction.wait()
-    let event=tx.events[0]
-    let value=event.args[2]
-    let tokenId=value.toNumber()
+    let contract = new ethers.Contract(nftaddress, NFT.abi, signer);
+    let transaction = await contract.createToken(url);
+    let tx = await transaction.wait();
+    let event = tx.events[0];
+    let value = event.args[2];
+    let tokenId = value.toNumber();
 
-    const price=ethers.utils.parseUnits(formInput.price,'ether')
+    const price = ethers.utils.parseUnits(formInput.price, "ether");
 
-    contract=new ethers.Contract(nftmarketaddress,NFTMarket.abi,signer)
-    let listingPrice=await contract.getListingPrice()
-    listingPrice=listingPrice.toString()
+    contract = new ethers.Contract(nftmarketaddress, NFTMarket.abi, signer);
+    let listingPrice = await contract.getListingPrice();
+    listingPrice = listingPrice.toString();
 
-    transaction=await contract.createMarketItem(nftaddress,tokenId,price,{value: listingPrice})
-    await transaction.wait()
-    router.push('/')
+    transaction = await contract.createMarketItem(nftaddress, tokenId, price, {
+      value: listingPrice,
+    });
+    await transaction.wait();
+    router.push("/");
   }
 
-  return(
+  return (
     <div className="flex justify-center">
       <div className="w-1/2 flex flex-col pb-12">
-        <input placeholder="NFT Name"
-        className="mt-8 border rounded p-4"
-        onChange={e => updateFormInput({...formInput,name:e.target.value})}/>
+        <input
+          placeholder="NFT Name"
+          className="mt-8 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, name: e.target.value })
+          }
+        />
 
-        <textarea 
-        placeholder="NFT Description"
-        className="mt-2 border rounded p-4"
-        onChange={e => updateFormInput({...formInput,description:e.target.value})}/>
-
+        <textarea
+          placeholder="NFT Description"
+          className="mt-2 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, description: e.target.value })
+          }
+        />
 
         <input
-        placeholder="NFT price in ETH"
-        className="mt-2 border rounded p-4"
-        onChange={e => updateFormInput({...formInput,price:e.target.value})}/>
+          placeholder="NFT price in ETH"
+          className="mt-2 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, price: e.target.value })
+          }
+        />
 
-        <input
-        type="file"
-        name="asset"
-        className="my-3"
-        onChange={onChange}/>
+        <input type="file" name="asset" className="my-3" onChange={onChange} />
 
-        {
-          fileUrl &&(
-            <img className="rounded mt-4" width="350" src={fileUrl} />
-          )
-        }
-        <button onClick={CreateMarket} className="font-bold mt-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white rounded p-4 shadow-lg">
+        {fileUrl && <img className="rounded mt-4" width="350" src={fileUrl} />}
+        <button
+          onClick={CreateMarket}
+          className="font-bold mt-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white rounded p-4 shadow-lg"
+        >
           Create NFT
         </button>
       </div>
     </div>
-  )
-
-};
+  );
+}
